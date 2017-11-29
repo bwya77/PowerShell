187bf2e7e0bed058a5750587c04ca77d4f18e611
Bradley Wyatt <bradleywyatt1@gmail.com>
2017-11-28 22:43:24 -0600

    Create Office365SKU_Friendly_Name.ps1

---
+++ Office365SKU_Friendly_Name.ps1
@@ -0,0 +1,147 @@
+$Sku = @{
+	"O365_BUSINESS_ESSENTIALS"		   = "Office 365 Business Essentials"
+	"O365_BUSINESS_PREMIUM"		       = "Office 365 Business Premium"
+	"DESKLESSPACK"					   = "Office 365 (Plan K1)"
+	"DESKLESSWOFFPACK"				   = "Office 365 (Plan K2)"
+	"LITEPACK"						   = "Office 365 (Plan P1)"
+	"EXCHANGESTANDARD"				   = "Office 365 Exchange Online Only"
+	"STANDARDPACK"					   = "Enterprise Plan E1"
+	"STANDARDWOFFPACK"				   = "Office 365 (Plan E2)"
+	"ENTERPRISEPACK"				   = "Enterprise Plan E3"
+	"ENTERPRISEPACKLRG"			       = "Enterprise Plan E3"
+	"ENTERPRISEWITHSCAL"			   = "Enterprise Plan E4"
+	"STANDARDPACK_STUDENT"			   = "Office 365 (Plan A1) for Students"
+	"STANDARDWOFFPACKPACK_STUDENT"	   = "Office 365 (Plan A2) for Students"
+	"ENTERPRISEPACK_STUDENT"		   = "Office 365 (Plan A3) for Students"
+	"ENTERPRISEWITHSCAL_STUDENT"	   = "Office 365 (Plan A4) for Students"
+	"STANDARDPACK_FACULTY"			   = "Office 365 (Plan A1) for Faculty"
+	"STANDARDWOFFPACKPACK_FACULTY"	   = "Office 365 (Plan A2) for Faculty"
+	"ENTERPRISEPACK_FACULTY"		   = "Office 365 (Plan A3) for Faculty"
+	"ENTERPRISEWITHSCAL_FACULTY"	   = "Office 365 (Plan A4) for Faculty"
+	"ENTERPRISEPACK_B_PILOT"		   = "Office 365 (Enterprise Preview)"
+	"STANDARD_B_PILOT"				   = "Office 365 (Small Business Preview)"
+	"VISIOCLIENT"					   = "Visio Pro Online"
+	"POWER_BI_ADDON"				   = "Office 365 Power BI Addon"
+	"POWER_BI_INDIVIDUAL_USE"		   = "Power BI Individual User"
+	"POWER_BI_STANDALONE"			   = "Power BI Stand Alone"
+	"POWER_BI_STANDARD"			       = "Power-BI Standard"
+	"PROJECTESSENTIALS"			       = "Project Lite"
+	"PROJECTCLIENT"				       = "Project Professional"
+	"PROJECTONLINE_PLAN_1"			   = "Project Online"
+	"PROJECTONLINE_PLAN_2"			   = "Project Online and PRO"
+    "ProjectPremium"                   = "Project Online Premium"
+	"ECAL_SERVICES"				       = "ECAL"
+	"EMS"							   = "Enterprise Mobility Suite"
+	"RIGHTSMANAGEMENT_ADHOC"		   = "Windows Azure Rights Management"
+	"MCOMEETADV"					   = "PSTN conferencing"
+	"SHAREPOINTSTORAGE"			       = "SharePoint storage"
+	"PLANNERSTANDALONE"			       = "Planner Standalone"
+	"CRMIUR"						   = "CMRIUR"
+	"BI_AZURE_P1"					   = "Power BI Reporting and Analytics"
+	"INTUNE_A"						   = "Windows Intune Plan A"
+    "PROJECTWORKMANAGEMENT"            = "Office 365 Planner Preview"
+    "ATP_ENTERPRISE"                   = "Exchange Online Advanced Threat Protection"
+    "EQUIVIO_ANALYTICS"                = "Office 365 Advanced eDiscovery"
+    "AAD_BASIC"                        = "Azure Active Directory Basic"
+    "RMS_S_ENTERPRISE"                 = "Azure Active Directory Rights Management"
+    "AAD_PREMIUM"                      = "Azure Active Directory Premium"
+    "MFA_PREMIUM"                      = "Azure Multi-Factor Authentication"
+    "STANDARDPACK_GOV"       	       = "Microsoft Office 365 (Plan G1) for Government"
+    "STANDARDWOFFPACK_GOV"	           = "Microsoft Office 365 (Plan G2) for Government"
+    "ENTERPRISEPACK_GOV"	           = "Microsoft Office 365 (Plan G3) for Government"
+    "ENTERPRISEWITHSCAL_GOV"     	   = "Microsoft Office 365 (Plan G4) for Government"
+    "DESKLESSPACK_GOV"	               = "Microsoft Office 365 (Plan K1) for Government"
+    "ESKLESSWOFFPACK_GOV"	           = "Microsoft Office 365 (Plan K2) for Government"
+    "EXCHANGESTANDARD_GOV"	           = "Microsoft Office 365 Exchange Online (Plan 1) only for Government"
+    "EXCHANGEENTERPRISE_GOV"	       = "Microsoft Office 365 Exchange Online (Plan 2) only for Government"
+    "SHAREPOINTDESKLESS_GOV"	       = "SharePoint Online Kiosk"
+    "EXCHANGE_S_DESKLESS_GOV"	       = "Exchange Kiosk"
+    "RMS_S_ENTERPRISE_GOV"	           = "Windows Azure Active Directory Rights Management"
+    "OFFICESUBSCRIPTION_GOV"       	   = "Office ProPlus"
+    "MCOSTANDARD_GOV"	               = "Lync Plan 2G"
+    "SHAREPOINTWAC_GOV"	               = "Office Online for Government"
+    "SHAREPOINTENTERPRISE_GOV"	       = "SharePoint Plan 2G"
+    "EXCHANGE_S_ENTERPRISE_GOV"    	   = "Exchange Plan 2G"
+    "EXCHANGE_S_ARCHIVE_ADDON_GOV"	   = "Exchange Online Archiving"
+    "EXCHANGE_S_DESKLESS"	           = "Exchange Online Kiosk"
+    "SHAREPOINTDESKLESS"	           = "SharePoint Online Kiosk"
+    "SHAREPOINTWAC"  	               = "Office Online"
+    "YAMMER_ENTERPRISE"	               = "Yammer for the Starship Enterprise"
+    "EXCHANGE_L_STANDARD"	           = "Exchange Online (Plan 1)"
+    "MCOLITE"	                       = "Lync Online (Plan 1)"
+    "SHAREPOINTLITE"	               = "SharePoint Online (Plan 1)"
+    "OFFICE_PRO_PLUS_SUBSCRIPTION_SMBIZ" = "Office ProPlus"
+    "EXCHANGE_S_STANDARD_MIDMARKET"	   = "Exchange Online (Plan 1)"
+    "MCOSTANDARD_MIDMARKET"	           = "Lync Online (Plan 1)"
+    "SHAREPOINTENTERPRISE_MIDMARKET"   = "SharePoint Online (Plan 1)"
+    "OFFICESUBSCRIPTION"	           = "Office ProPlus"
+    "YAMMER_MIDSIZE"	               = "Yammer"
+    "DYN365_ENTERPRISE_PLAN1"          = "Dynamics 365 Customer Engagement Plan Enterprise Edition"
+    "ENTERPRISEPREMIUM_NOPSTNCONF"     = "Enterprise E5 (without Audio Conferencing)"
+    "ENTERPRISEPREMIUM"                = "Enterprise E5 (with Audio Conferencing)"
+    "MCOSTANDARD"                      = "Skype for Business Online Standalone Plan 2"
+    "PROJECT_MADEIRA_PREVIEW_IW_SKU"   = "Dynamics 365 for Financials for IWs"
+}
+$Cred = Get-Credential
+Connect-MsolService -Credential $Cred
+$CSV = "C:\Scripts\Office_365_User_Licensing4.csv"
+$Users = MsolUser | Where-Object { $_.isLicensed -eq "TRUE" } | Sort-Object DisplayName
+Foreach ($User in $Users)
+{
+	Write-Host "Working on $($User.DisplayName)..." -ForegroundColor Yellow
+	#Gets users license and splits it at the semicolon
+	Write-Host "Getting all licenses for $($User.DisplayName)..." -ForegroundColor White
+	$Licenses = ((Get-MsolUser -UserPrincipalName $User.UserPrincipalName).Licenses).AccountSkuID
+	If (($Licenses).Count -gt 1)
+	{
+		Foreach ($License in $Licenses)
+		{
+			Write-Host "Finding $License in the Hash Table..." -ForegroundColor White
+			$LicenseItem = $License -split ":" | Select-Object -Last 1
+			$TextLic = $Sku.Item("$LicenseItem")
+			If (!($TextLic))
+			{
+				Write-Host "Error: The Hash Table has not match for $LicenseItem for $($User.DisplayName)!" -ForegroundColor Red
+			}
+			Else
+			{
+				
+				$NewObject02 = $null
+				$NewObject02 = @()
+				$NewObject01 = New-Object PSObject
+				$NewObject01 | Add-Member -MemberType NoteProperty -Name "Name" -Value $User.DisplayName
+				$NewObject01 | Add-Member -MemberType NoteProperty -Name "User Principal Name" -Value $User.UserPrincipalName
+				$NewObject01 | Add-Member -MemberType NoteProperty -Name "License" -Value "$TextLic"
+				$NewObject02 += $NewObject01
+				$NewObject02 | Export-CSV $CSV -NoTypeInformation -Append
+				
+			}
+		}
+		
+	}
+	Else
+	{
+		Write-Host "Finding $Licenses in the Hash Table..." -ForegroundColor White
+		$LicenseItem = ((Get-MsolUser -UserPrincipalName $User.UserPrincipalName).Licenses).AccountSkuID -split ":" | Select-Object -Last 1
+		$TextLic = $Sku.Item("$LicenseItem")
+		If (!($TextLic))
+		{
+			Write-Host "Error: The Hash Table has not match for $LicenseItem for $($User.DisplayName)!" -ForegroundColor Red
+		}
+		Else
+		{
+			$NewObject02 = $null
+			$NewObject02 = @()
+			$NewObject01 = New-Object PSObject
+			$NewObject01 | Add-Member -MemberType NoteProperty -Name "Name" -Value $User.DisplayName
+			$NewObject01 | Add-Member -MemberType NoteProperty -Name "User Principal Name" -Value $User.UserPrincipalName
+			$NewObject01 | Add-Member -MemberType NoteProperty -Name "License" -Value "$TextLic"
+			$NewObject02 += $NewObject01
+			$NewObject02 | Export-CSV $CSV -NoTypeInformation -Append
+		}
+	}
+}
+
+#Remove PSSession
+Get-PSSession | Remove-PSSession
+
