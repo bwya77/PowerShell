cd9d788f85a295001d4a228b75b7d3802bc8b5b3
Bradley Wyatt <bradleywyatt1@gmail.com>
2020-10-07 14:42:14 -0500

    Add files via upload

---
+++ FailedLogons.ps1
@@ -0,0 +1,39 @@
+﻿$myObject = @()
+
+$EventLogs = Get-WinEvent -ComputerName localhost -FilterHashtable @{
+    LogName = 'Security'
+    ID = 4625
+}
+
+Foreach ($event in $EventLogs)
+{
+[string]$Item = $Event.Message 
+
+$AccountName = $Item.SubString($Item.IndexOf("Account For Which Logon Failed:"))
+$AccountName = $AccountName.SubString($AccountName.IndexOf("	Account Name:"))
+$AccountName = ($AccountName -split ':')[1]
+$AccountName = ($AccountName -split '\n')[0]
+$AccountName = $AccountName.trim()
+
+
+
+
+$Reason = $Item.SubString($Item.IndexOf("Failure Reason:"))
+$Reason = $Reason.SubString($Reason.IndexOf("Failure Reason:"))
+$Reason = ($Reason -split ':')[1]
+$Reason = ($Reason -split '\n')[0]
+$Reason = $Reason.trim()
+
+$myObject += [PSCustomObject]@{
+    TimeCreated     = $($Event.TimeCreated)
+    ID              = $($Event.ID)
+    User            = $AccountName
+    Reason          = $Reason
+}
+}
+
+
+
+
+
+
